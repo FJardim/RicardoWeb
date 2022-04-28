d0e09bbad395f92ad0714ce20cd7398bb26fff5b
FJardim <licdafeliciajardim0305@gmail.com>
2022-04-28 17:29:04 -0400

    feature (PopularSearch, WeightPlan, SwiperPopular: craete new Component for Home)

--- src/componentes/CategorySectionCard.js
+++ src/componentes/CategorySectionCard.js
@@ -1,10 +1,10 @@
-const CategorySectionCard = (props, img) => {
+const CategorySectionCard = ({ img, name }) => {
     return (
-        <div className="relative h-70 w-[900px] rounded-md transition transform hover:-translate-y-1 hover:drop-shadow-2xl duration-300 cursor-pointer" style={{ backgroundImage: `url(${img})` }}>
+        <div className="mt-10 mb-10 relative h-70 w-full rounded-md transition transform hover:-translate-y-1 hover:drop-shadow-2xl duration-300 cursor-pointer" style={{ backgroundImage: `url(${img})` }}>
             <div className='rounded-md absolute bg-black h-full w-full opacity-30' >
             </div>
             <div className="relative text-white text-center py-10 text-2xl" style={{ textShadow: "0px 0px 3px #000000" }} >
-                {props.name}
+                {name}
 
             </div>
         </div>
