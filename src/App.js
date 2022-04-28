ffcec4fdf0b651e25c00d49cf0c10988dba80aed
FJardim <licdafeliciajardim0305@gmail.com>
2022-04-28 10:23:02 -0400

    feature (RemoveImports): remove unnecessary imports on file App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import './App.css';
-import NavBar from './componentes/NavBar';
-import NavInfo from './componentes/NavInfo';
-import SwiperHome from './componentes/SwiperHome';
-import Card from './componentes/Card';
-import Footer from './componentes/Footer';
-import CategorySectionCard from './componentes/CategorySectionCard';
 import {
   BrowserRouter,
   Routes,
@@ -19,13 +13,13 @@ function App() {
 
   return (
     <AppLayout>
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipes" element={<Recipes />} />
         </Routes>
-        </BrowserRouter>
-    </AppLayout>    
+      </BrowserRouter>
+    </AppLayout>
   );
 }
 
