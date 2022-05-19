87b367cbaf921e321fb8f907e8f19764ec5090d4
FJardim <licdafeliciajardim0305@gmail.com>
2022-05-19 17:26:23 -0400

    refactor(cardGym, ProductInfo)

--- src/componentes/CardGym.js
+++ src/componentes/CardGym.js
@@ -8,7 +8,7 @@ const BannerChef = ({ foto, name, description }) => {
         style={{ background: `url(${foto})`, backgroundSize: "100% 100%" }}
       >
         <div className="flex -mr-36">
-          <img src={PremiumIcon} className="h-16 w-16 mb-6" />
+          <img src={PremiumIcon} className="h-16 w-16 mb-6" alt="" />
         </div>
 
         <div className="text-center text-white font-sans">
