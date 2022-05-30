be2cd9ae8c44cf695ed9914b17362dae44e22d48
luis <voldelord26@gmail.com>
2022-05-30 12:53:33 -0400

    feature (pages) updating sellers buttombuttom menuleft

--- src/componentes/ScrollNavigation.js
+++ src/componentes/ScrollNavigation.js
@@ -1,16 +1,70 @@
+import React from "react";
 
-const ScrollNavigation = ({ title }) => {
+const ButtomButton = () => {
   return (
-    <div>
-      <button
-        className="inline-flex items-center 
-        justify-center cursor-pointer 
-        w-12 h-12 font-semibold hover:bg-main hover:text-white border border-main
-        transition duration-300 rounded-full"
-      >
-        {title}
-      </button>
+    <div class="flex flex-col items-center my-12">
+      <div class="flex text-gray-700">
+        <div class="h-12 w-12 mr-1 flex justify-center items-center rounded-full cursor-pointer  hover:bg-main hover:text-white">
+          <svg
+            xmlns=""
+            width="100%"
+            height="100%"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-chevron-left w-6 h-6"
+          >
+            <polyline points="15 18 9 12 15 6"></polyline>
+          </svg>
+        </div>
+        <div class="flex h-12 font-medium rounded-full space-x-2">
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  border border-main hover:bg-main hover:text-white">
+            1
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full border border-main  hover:bg-main hover:text-white ">
+            2
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full border border-main  hover:bg-main hover:text-white  ">
+            3
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full ">
+            ...
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full border border-main  hover:bg-main hover:text-white ">
+            8
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full border border-main  hover:bg-main hover:text-white ">
+            9
+          </div>
+          <div class="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full border border-main  hover:bg-main hover:text-white ">
+            10
+          </div>
+          <div class="w-12 h-12 md:hidden flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-full bg-main text-white ">
+            1
+          </div>
+        </div>
+        <div class="h-12 w-12 ml-1 flex justify-center items-center rounded-full cursor-pointer hover:bg-main hover:text-white">
+          <svg
+            xmlns=""
+            width="100%"
+            height="100%"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-chevron-right w-6 h-6"
+          >
+            <polyline points="9 18 15 12 9 6"></polyline>
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
-export default ScrollNavigation;
+
+export default ButtomButton;
