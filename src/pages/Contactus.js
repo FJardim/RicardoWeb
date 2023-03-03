39d986fccaa735a8a7737ca09e569e31ab297711
Alexis Navarro <alexisnavarro1994@gmail.com>
2023-03-03 13:47:22 -0400

    feat(contact us): add contact information

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -2,12 +2,17 @@ import BannerPage from "../componentes/BannerPage";
 import foodBg from "../assets/img1.jpg";
 import { useState } from "react";
 import useAxios from "../hooks/useAxios";
+import {BsPersonFill} from "react-icons/bs";
+import {ImSpinner9} from "react-icons/im";
+import {IoLocationSharp, IoMail} from "react-icons/io5";
+
+const defaultData = () => ({
+  email: '',
+  content: '',
+})
 
 const Contactus = () => {
-  const [data, setData] = useState({
-    email: '',
-    content: '',
-  });
+  const [data, setData] = useState(defaultData());
 
   const [{ data: contactData, loading }, sendEmail] = useAxios({ url: '/email-contacts', method: 'POST' }, { manual: 'true' });
 
@@ -18,10 +23,12 @@ const Contactus = () => {
     }));
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     
-    sendEmail({ data });
+    await sendEmail({ data });
+
+    setData(defaultData())
   }
   
   return (
@@ -29,56 +36,62 @@ const Contactus = () => {
       <BannerPage image={foodBg} title="Contact us" />
 
       <div className="container md:p-20 p-4 h-full md:w-md mb-6 max-w-full">
-        {contactData
-          ? <div className="bg-white p-4 md:p-10 rounded-lg md:h-full w-full shadow text-center">
-            <h3 className="text-3xl font-semibold text-main">Message sent successfully!</h3>
+        <p className="md:text-4xl text-2xl text-center md:text-justify font-bold text-black mb-6 md:mb-12">
+          How can we help you?
+        </p>
+        <div className="flex">
+          <form className="bg-white p-4 md:p-10 rounded-lg md:h-full w-full shadow" onSubmit={handleSubmit}>
+            {contactData && <div className="flex justify-between items-center px-4 py-3 border border-main rounded text-main">
+              <span>Message sent successfully!</span>
+            </div>}
+            
+            <div className="my-6">
+              <label className="text-sm text-gray-400" htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="
+                    first-letter:block
+                    w-full
+                    rounded-lg 
+                    border-gray-200
+                    focus:ring-0 
+                    focus:border-gray-200
+                "
+                placeholder="xxxx@example.com..."
+                id="email"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+              />
+            </div>
+            <label className="text-sm text-gray-400" htmlFor="content">Leave us your message</label>
+            <textarea
+              className=" border-gray-200 focus:ring-0 focus:border-gray-200 rounded-lg w-full text-sm"
+              placeholder="Your Message..."
+              id="content"
+              name="content"
+              value={data.content}
+              onChange={handleChange}
+              rows={4}
+            ></textarea>
+            <div className="m-4 text-right">
+              <button
+                className="bg-main text-white px-6 py-2 hover:bg-main-light font-semibold rounded-lg"
+                disabled={loading}
+              >
+                {loading ? <ImSpinner9 className="animate-spin" /> : 'Send Message'}
+              </button>
+            </div>
+          </form>
+
+          <div className="w-[350px] pl-6 text-lg text-gray-700">
+            <p className="flex leading-none mb-4"><BsPersonFill className="mr-1 flex-shrink-0" /> Ric's consulting</p>
+
+            <p className="flex leading-none mb-4"><IoLocationSharp className="mr-1 flex-shrink-0" /> PO box 28684 Sandy Springs, GA 30358</p>
+
+            <p className="flex leading-none mb-4"><IoMail className="mr-1 flex-shrink-0" /> ricsconsulting88@gmail.com</p>
           </div>
-          : (
-            <>
-              <p className="md:text-4xl text-2xl text-center md:text-justify font-bold text-black mb-6 md:mb-12">
-                How can we help you?
-              </p>
-              <form className="bg-white p-4 md:p-10 rounded-lg md:h-full w-full shadow" onSubmit={handleSubmit}>
-                <div className="my-6">
-                  <label className="text-sm text-gray-400" htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    className="
-                        first-letter:block
-                        w-full
-                        rounded-lg 
-                        border-gray-200
-                        focus:ring-0 
-                        focus:border-gray-200
-                    "
-                    placeholder="xxxx@example.com..."
-                    id="email"
-                    name="email"
-                    value={data.email}
-                    onChange={handleChange}
-                  />
-                </div>
-                <label className="text-sm text-gray-400" htmlFor="content">Leave us your message</label>
-                <textarea
-                  className=" border-gray-200 focus:ring-0 focus:border-gray-200 rounded-lg w-full text-sm"
-                  placeholder="Your Message..."
-                  id="content"
-                  name="content"
-                  value={data.content}
-                  onChange={handleChange}
-                  rows={4}
-                ></textarea>
-                <div className="m-4 text-right">
-                  <button
-                    className="bg-main text-white px-6 py-2 hover:bg-main-light font-semibold rounded-lg"
-                    disabled={loading}
-                  >
-                    Send Message
-                  </button>
-                </div>
-              </form>
-            </>
-          )}
+        </div>
       </div>
     </div>
   );
