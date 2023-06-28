0fdbd922e455d3168c56431d1cdcec754ebd3a35
Alexis Navarro <alexisnavarro1994@gmail.com>
2023-06-28 13:57:57 -0400

    feature: order products by createdAt

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -16,7 +16,6 @@ import CategoriesRecipes from "../componentes/CategoriesRecipes";
 import RatingComponent from "../componentes/RatingComponent";
 
 const Plans = () => {
-
   const { user } = useAuth();
 
   const [showModalMenu, setShowModalMenu] = useState(false);
@@ -26,30 +25,34 @@ const Plans = () => {
   const [plansFilters, setPlansFilters] = useState({
     page: 1,
     perPage: 9,
-    name: '',
+    name: "",
     categoryIds: [],
     hideFavoritedForClientId: user?.id,
-    hideClientPlans: 'true',
-    rating: '',
-    orderByMostPurchased: ''
+    hideClientPlans: "true",
+    rating: "",
+    orderByMostPurchased: "",
+    orderBy: "createdAt,DESC",
   });
 
-  const [{ plans, numberOfPages, loading }, getPlans] = usePlans({ params: { ...plansFilters }, options: { useCache: false } });
+  const [{ plans, numberOfPages, loading }, getPlans] = usePlans({
+    params: { ...plansFilters },
+    options: { useCache: false },
+  });
 
   useEffect(() => {
-    const categoryId = searchParams?.get('categoryId');
+    const categoryId = searchParams?.get("categoryId");
 
-    const name = searchParams?.get('name');
+    const name = searchParams?.get("name");
 
-    const orderByMostPurchased = searchParams?.get('orderByMostPurchased');
+    const orderByMostPurchased = searchParams?.get("orderByMostPurchased");
 
     if (categoryId) {
       setPlansFilters((oldFilters) => {
         return {
           ...oldFilters,
           categoryIds: [categoryId],
-          page: 1
-        }
+          page: 1,
+        };
       });
     }
 
@@ -58,8 +61,8 @@ const Plans = () => {
         return {
           ...oldFilters,
           name: name,
-          page: 1
-        }
+          page: 1,
+        };
       });
     }
 
@@ -68,11 +71,10 @@ const Plans = () => {
         return {
           ...oldFilters,
           orderByMostPurchased: true,
-          page: 1
-        }
+          page: 1,
+        };
       });
     }
-
   }, [searchParams]);
 
   const handleCategory = (category) => {
@@ -80,29 +82,32 @@ const Plans = () => {
       return {
         ...oldPlansFilters,
         page: 1,
-        categoryIds: category?.id ? [category?.id] : []
-      }
+        categoryIds: category?.id ? [category?.id] : [],
+      };
     });
-  }
+  };
 
   return (
     <div className="">
       <BannerPage image={img1} title="Plans" />
-      <div className="container md:p-8">
-      </div>
+      <div className="container md:p-8"></div>
       <div className="p-6">
-
         <ButtonOverview name="Filter" onClick={() => setShowModalMenu(true)} />
         <p className="my-4"></p>
-        <ButtonOverview name="Clear Filters" onClick={() => setPlansFilters({
-          page: 1,
-          perPage: 9,
-          name: '',
-          categoryIds: [],
-          hideFavoritedForClientId: user?.id,
-          hideClientPlans: 'true',
-          rating: ''
-        })} />
+        <ButtonOverview
+          name="Clear Filters"
+          onClick={() =>
+            setPlansFilters({
+              page: 1,
+              perPage: 9,
+              name: "",
+              categoryIds: [],
+              hideFavoritedForClientId: user?.id,
+              hideClientPlans: "true",
+              rating: "",
+            })
+          }
+        />
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-2">
           <MenuLeft
             filters={plansFilters}
@@ -112,46 +117,33 @@ const Plans = () => {
                 return {
                   ...oldPlansFilters,
                   rating: rating,
-                  page: 1
-                }
-              })
+                  page: 1,
+                };
+              });
             }}
-
           />
 
           <div className="mt-10 md:mt-0 md:col-span-3">
-            {
-              plansFilters?.orderByMostPurchased &&
+            {plansFilters?.orderByMostPurchased && (
               <div className="text-center text-4xl mb-16">
                 ⭐ Most Popular Plans ⭐
               </div>
-            }
-            {
-              plansFilters?.name &&
+            )}
+            {plansFilters?.name && (
               <div className="text-center text-4xl mb-16">
                 Results for "{plansFilters?.name}..."
               </div>
-            }
-            {
-              loading &&
-              <h1 className="text-4xl text-center">
-                loading...
+            )}
+            {loading && <h1 className="text-4xl text-center">loading...</h1>}
+            {plans?.length === 0 && !loading ? (
+              <h1 className="text-4xl text-center text-red-500">
+                No results found.
               </h1>
-            }
-            {
-              plans?.length === 0 && !loading ?
-                <h1 className="text-4xl text-center text-red-500">
-                  No results found.
-                </h1>
-                :
-                null
-            }
+            ) : null}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-2 lg:grid-cols-3 md:mr-5">
               {plans.map((plan, i) => {
                 return (
-                  <Link
-                    key={i}
-                    to={`/plan/${plan?.slug}`}>
+                  <Link key={i} to={`/plan/${plan?.slug}`}>
                     <WeightPlan
                       price={`${plan?.price}$`}
                       hideCart
@@ -168,16 +160,26 @@ const Plans = () => {
             <br />
             <Pagination
               pages={numberOfPages}
-              onChange={(page) => setPlansFilters((oldFilters) => { return { ...oldFilters, page: page } })}
+              onChange={(page) =>
+                setPlansFilters((oldFilters) => {
+                  return { ...oldFilters, page: page };
+                })
+              }
               activePage={plansFilters?.page}
             />
           </div>
         </div>
       </div>
-      <ModalOverlay onClose={() => setShowModalMenu(false)} show={showModalMenu}  >
+      <ModalOverlay
+        onClose={() => setShowModalMenu(false)}
+        show={showModalMenu}
+      >
         <ModalContainer onClose={() => setShowModalMenu(false)}>
           <div className="rounded-lg shadow">
-            <CategoriesRecipes onClickCategory={handleCategory} values={plansFilters?.categoryIds} />
+            <CategoriesRecipes
+              onClickCategory={handleCategory}
+              values={plansFilters?.categoryIds}
+            />
           </div>
           <div className="p-4 mt-6 bg-white m-auto md:w-40 rounded-lg shadow">
             <h1 className="title-medium mt-2 mb-6">Rating</h1>
@@ -188,33 +190,33 @@ const Plans = () => {
                     return {
                       ...oldPlansFilters,
                       rating: rating,
-                      page: 1
-                    }
-                  })
+                      page: 1,
+                    };
+                  });
                 }}
                 value={plansFilters?.rating}
               />
-              {
-                plansFilters?.rating &&
-                <button className="bg-main rounded-xl text-white px-4 py-1"
+              {plansFilters?.rating && (
+                <button
+                  className="bg-main rounded-xl text-white px-4 py-1"
                   onClick={() => {
                     setPlansFilters((oldPlansFilters) => {
                       return {
                         ...oldPlansFilters,
-                        rating: '',
-                        page: 1
-                      }
-                    })
+                        rating: "",
+                        page: 1,
+                      };
+                    });
                   }}
                 >
                   Clear
                 </button>
-              }
+              )}
             </div>
           </div>
         </ModalContainer>
       </ModalOverlay>
-    </div >
+    </div>
   );
 };
 
