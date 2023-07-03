51ee40d9ae826a81bd0e04daea7ee35f649436bd
Alexis Navarro <alexisnavarro1994@gmail.com>
2023-07-03 11:54:37 -0400

    feature: add REACT_APP_HOST_URL env variable

--- src/util/SystemInfo.js
+++ src/util/SystemInfo.js
@@ -2,14 +2,14 @@ import logoBlanco from "../assets/logo-blanco.png";
 import logoShort from "../assets/logoR.png";
 
 const SystemInfo = {
-    name: "Recipes",
-    description: "Find your favorites recipes and shared it with your friends.",
-    logo: logoShort,
-    logoBlanco: logoBlanco,
-    logoShort: logoShort,
-    host: 'http://localhost::3000',
-    api: process.env.REACT_APP_API_URL,
-    localStorageVarPrefix: 'recipes_clients_'
-}
+  name: "Recipes",
+  description: "Find your favorites recipes and shared it with your friends.",
+  logo: logoShort,
+  logoBlanco: logoBlanco,
+  logoShort: logoShort,
+  host: process.env.REACT_APP_HOST_URL,
+  api: process.env.REACT_APP_API_URL,
+  localStorageVarPrefix: "recipes_clients_",
+};
 
 export default SystemInfo;
